0501faa9f1d5bc17b600cd63567bf4120b955188
양승길 <y6mm@naver.com>
2020-07-21 20:23:03 +0900

    build
    build

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController// 1
 public class HelloController {
+
+    @GetMapping("hello") // 2
+    public String hello() {
+        return "hello";
+    }
+
     @GetMapping("/hello/dto")
     public HelloResponseDto helloDto(@RequestParam("name")String name
             , @RequestParam("amount")int amount){ // [1]
